c73a41ca20da75bd486a33764d8eeb634db2e616
Hae <duynguyen11062002@gmail.com>
2024-01-03 08:57:50 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3306;
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(morgan("combined"));
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+route(app);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
